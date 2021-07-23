63af0aa4a77416b2ad7142e109fdbd06ecd8327c
jungle <dgjungle_c@163.com>
2021-07-23 22:45:31 +0800

    day4

---
+++ src/easy/IsCovered.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class IsCovered {
+
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        boolean[] flag = new boolean[51];
+        for(int[] range : ranges){
+            for(int i = range[0]; i <= range[1];i++){
+                flag[i] = true;
+            }
+        }
+        for(int i = left; i <= right; i++){
+            if(flag[i] == false) return false;
+        }
+        return true;
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
